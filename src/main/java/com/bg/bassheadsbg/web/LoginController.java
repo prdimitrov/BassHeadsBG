0bb76f2f2245f2392c207dddf0736aae638d1200
prdimitrov <prdimitrov97@gmail.com>
2024-07-03 17:36:35 +0300

    - Added footer to some of the pages - Added a LoginController.

---
+++ src/main/java/com/bg/bassheadsbg/web/LoginController.java
@@ -0,0 +1,14 @@
+package com.bg.bassheadsbg.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class LoginController {
+    @GetMapping("/login")
+    public String login() {
+        return "auth-login";
+    }
+}
