71db24e0f30bc9e5a09b69c1d91dd6513395b840
prdimitrov <prdimitrov97@gmail.com>
2024-07-08 17:03:04 +0300

    - Added a RestClient and MessageSource to the configuration.

---
+++ src/main/java/com/bg/bassheadsbg/config/RestConfig.java
@@ -0,0 +1,14 @@
+package com.bg.bassheadsbg.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestConfig {
+
+    @Bean
+    public RestClient restClient() {
+        return RestClient.create();
+    }
+}
