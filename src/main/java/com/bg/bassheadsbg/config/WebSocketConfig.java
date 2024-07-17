b567f91913d4c204458b3b1ddeb82296bf613ee9
prdimitrov <prdimitrov97@gmail.com>
2024-07-17 11:56:51 +0300

    - Reworking on the chat

--- src/main/java/com/bg/bassheadsbg/config/WebSocketConfig.java
+++ src/main/java/com/bg/bassheadsbg/config/WebSocketConfig.java
@@ -11,13 +11,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat-websocket").withSockJS();
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
     }
 }
--- src/main/java/com/bg/bassheadsbg/model/dto/ChatMessage.java
+++ src/main/java/com/bg/bassheadsbg/model/dto/ChatMessage.java
@@ -1,22 +1,16 @@
 package com.bg.bassheadsbg.model.dto;
 
+import com.bg.bassheadsbg.model.enums.MessageType;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ChatMessage {
     private String content;
     private String sender;
+    private MessageType messageType;
 
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getSender() {
-        return sender;
-    }
-
-    public void setSender(String sender) {
-        this.sender = sender;
-    }
 }
