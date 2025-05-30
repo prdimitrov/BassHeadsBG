7d14f664e31227c2aea5d93f0811584b5f96ea8d
DASKALA <prdimitrov97@gmail.com>
2025-05-30 11:28:14 +0300

    - Refactor classes code in aop folder - Refactor classes code in config and config/custom folders - Add constants.

--- src/main/java/com/bg/bassheadsbg/aop/InitializationAspect.java
+++ src/main/java/com/bg/bassheadsbg/aop/InitializationAspect.java
@@ -11,10 +11,14 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 public class InitializationAspect {
 
-    @Before("execution(* com.bg.bassheadsbg.init..*(..))")
+    private static final String TARGET = "{}";
+    private static final String EXECUTION_COM_BG_BASSHEADSBG_INIT = "execution(* com.bg.bassheadsbg.init..*(..))";
+    private static final String INITIALIZATION_TASK_MESSAGE = "Initialization task is about to start in class: " + TARGET + " and method: " + TARGET;
+
+    @Before(EXECUTION_COM_BG_BASSHEADSBG_INIT)
     public void logBeforeInitialization(JoinPoint joinPoint) {
         String className = joinPoint.getSignature().getDeclaringTypeName();
         String methodName = joinPoint.getSignature().getName();
-        log.info("Initialization task is about to start in class: {} and method: {}", className, methodName);
+        log.info(INITIALIZATION_TASK_MESSAGE, className, methodName);
     }
 }
