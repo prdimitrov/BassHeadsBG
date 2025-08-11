4d3beecaaac4e08ebf116e78404c2338f8ba291c
DASKALA <prdimitrov97@gmail.com>
2025-08-11 12:37:16 +0300

    - Refactor MultiChannelAmplifier device related classes.

--- src/main/java/com/bg/bassheadsbg/service/interfaces/MultiChannelAmpService.java
+++ src/main/java/com/bg/bassheadsbg/service/interfaces/MultiChannelAmpService.java
@@ -4,26 +4,7 @@ import com.bg.bassheadsbg.model.dto.add.AddMultiChannelAmpDTO;
 import com.bg.bassheadsbg.model.dto.details.MultiChannelAmpDetailsDTO;
 import com.bg.bassheadsbg.model.dto.summary.MultiChannelAmpSummaryDTO;
 import com.bg.bassheadsbg.model.helpers.MultiChannelAmpDetailsHelperDTO;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.util.List;
-
-public interface MultiChannelAmpService {
-
-    AddMultiChannelAmpDTO createNewAmplifier();
-
-    long addAmplifier(AddMultiChannelAmpDTO addMultiChannelAmpDTO) throws IOException;
-
-    long editAmplifier(AddMultiChannelAmpDTO addMultiChannelAmpDTO, List<MultipartFile> multipartFiles) throws IOException;
-
-    void deleteAmplifier(long amplifierId);
-
-    List<MultiChannelAmpSummaryDTO> getAllAmplifiersSummarySorted();
-
-    MultiChannelAmpDetailsDTO getAmplifierDetails(Long id);
-
-    MultiChannelAmpDetailsHelperDTO getAmplifierDetailsHelper(Long id);
-
-    boolean likeAmplifier(Long id);
+public interface MultiChannelAmpService extends DeviceService<
+        AddMultiChannelAmpDTO, MultiChannelAmpDetailsDTO, MultiChannelAmpSummaryDTO, MultiChannelAmpDetailsHelperDTO> {
 }
